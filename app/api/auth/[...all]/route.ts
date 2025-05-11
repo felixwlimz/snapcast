1ede29339ed512972ad48b7c9c5bf6b70a4cc7c0
felixwlimz <felixlim140@gmail.com>
2025-05-11 21:29:05 +0700

    Fix Route issues

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,11 +1,14 @@
-import aj from "@/lib/arcjet";
-import { auth } from "@/lib/auth";
-import { slidingWindow, validateEmail } from "@arcjet/next";
+import aj, {
+  ArcjetDecision,
+  shield,
+  slidingWindow,
+  validateEmail,
+} from "@/lib/arcjet";
 import ip from "@arcjet/ip";
+import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
 
-// Email Validation -> Block
 const emailValidation = aj.withRule(
   validateEmail({
     mode: "LIVE",
@@ -13,7 +16,6 @@ const emailValidation = aj.withRule(
   })
 );
 
-// Rate Limit
 const rateLimit = aj.withRule(
   slidingWindow({
     mode: "LIVE",
@@ -23,50 +25,55 @@ const rateLimit = aj.withRule(
   })
 );
 
-const protectedAuth = async (req: NextRequest) => {
+const shieldValidation = aj.withRule(
+  shield({
+    mode: "LIVE",
+  })
+);
+
+const protectedAuth = async (req: NextRequest): Promise<ArcjetDecision> => {
   const session = await auth.api.getSession({
     headers: req.headers,
   });
-
   let userId: string;
-
-  if (session?.user?.id) {
+  if (session?.user.id) {
     userId = session.user.id;
   } else {
     userId = ip(req) || "127.0.0.1";
   }
-
   if (req.nextUrl.pathname.startsWith("/api/auth/sign-in")) {
     const body = await req.clone().json();
-
     if (typeof body.email === "string") {
-      return emailValidation.protect(req, { email: body.email });
+      return emailValidation.protect(req, {
+        email: body.email,
+      });
     }
   }
-
-  return rateLimit.protect(req, { fingerprint: userId });
+  if (!req.nextUrl.pathname.startsWith("/api/auth/sign-out")) {
+    return rateLimit.protect(req, {
+      fingerprint: userId,
+    });
+  }
+  return shieldValidation.protect(req);
 };
 
-const authHandler = toNextJsHandler(auth.handler);
+const authHandlers = toNextJsHandler(auth.handler);
 
-export const { GET } = authHandler;
+export const { GET } = authHandlers;
 
 export const POST = async (req: NextRequest) => {
   const decision = await protectedAuth(req);
-
   if (decision.isDenied()) {
     if (decision.reason.isEmail()) {
       throw new Error("Email validation failed");
     }
-
     if (decision.reason.isRateLimit()) {
       throw new Error("Rate limit exceeded");
     }
-
     if (decision.reason.isShield()) {
-      throw new Error("Shield protection turned on");
+      throw new Error("Shield validation failed");
     }
   }
 
-  return authHandler.POST(req);
+  return authHandlers.POST(req);
 };
